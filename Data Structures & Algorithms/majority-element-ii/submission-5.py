217ab176f07bca0689f785363850ba8801880f04
Disha Handa <disha55handa@gmail.com>
2026-04-29 00:24:28 +0530

    Add: majority-element-ii - submission-5

---
+++ Data Structures & Algorithms/majority-element-ii/submission-5.py
@@ -0,0 +1,33 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        res1, res2 = -1, -1
+        cnt1, cnt2 = 0, 0
+        for num in nums:
+            if num == res1:
+                cnt1+=1
+            elif num == res2:
+                cnt2+=1
+            elif cnt1==0:
+                res1=num
+                cnt1=1
+            elif cnt2==0:
+                res2=num
+                cnt2=1
+            else:
+                cnt1-=1
+                cnt2-=1
+
+        cnt1, cnt2 = 0, 0
+        for i in range(len(nums)):
+            if nums[i] == res1:
+                cnt1+=1
+            elif nums[i]==res2:
+                cnt2+=1
+        
+        ans = []
+        n = len(nums)
+        if cnt1>n//3:
+            ans.append(res1)
+        if cnt2>n//3:
+            ans.append(res2)
+        return ans
