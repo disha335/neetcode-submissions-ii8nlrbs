76c18ce87a2675430f3c299e0cb5aebcd98e6308
Disha Handa <disha55handa@gmail.com>
2026-09-07 22:15:26 +0530

    Add: n-th-tribonacci-number - submission-5

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        # memo={0:0,1:1,2:1}
+        # def helper(i):
+        #     if i in memo:
+        #         return memo[i]
+        #     memo[i] = helper(i-1)+helper(i-2)+helper(i-3)
+        #     return memo[i]
+        # return helper(n)
+        # memo={0:0,1:1,2:1}
+        dp=[0]*(n+1)
+        if n==0:
+            return 0
+        if n==1 or n==2:
+            return 1
+        dp[1]=1
+        dp[2]=1
+        for i in range(3,n+1):
+            dp[i] = dp[i-1]+dp[i-2]+dp[i-3]
+        return dp[n]
